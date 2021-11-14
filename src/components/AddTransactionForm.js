dc92c8565b670e49bec9cfe09473a53041d6a9e3
Sumaya <sumaya.ahmad119@gmail.com>
2021-11-14 21:55:50 +0200

    define setName() and setAmount()

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -7,17 +7,28 @@ export const AddTransactionForm = () => {
     const [amount, setAmount] = useState(0);
     const { addTransaction } = useContext(GlobalContext);
 
+    const onSubmit = (e) => {
+        e.preventDefault();  // disable page reload on form submit
+        
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000),
+            name,
+            amount: +amount // add up to the previous amount
+        };
+        addTransaction(newTransaction); 
+    }
+
     return (
         <>
             <h3>Add New Transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}> {/* first onSubmit is built-in js, the second onSubmit, is the predefined function above */}
                 <div className="form-group">
                     <label htmlFor="text">Name</label>
-                    <input type="text" className="form-control" placeholder="Enter name here" />
+                    <input type="text" className="form-control" placeholder="Enter name here" onChange={(e) => {setName(e.target.value)}} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="amount">(<b>-</b>/<b>+</b>)Amount &npsp; &npsp; [-expense, +income]</label>                                                                                                                                                           ';,m öó¬í'
-                    <input type="number" placeholder="Enter amount" />
+                    <input type="number" placeholder="Enter amount" onChange={(e) => {setAmount(e.target.value)}} />
                 </div>
                 <button className="btn">Add</button>
             </form>
